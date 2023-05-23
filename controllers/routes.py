512ba16c5006ac63b5822e28fc30a9325a4357c9
davitorress <dev.davitorres@gmail.com>
2023-05-23 17:40:31 -0300

    feat: pancs page, route and style

--- controllers/routes.py
+++ controllers/routes.py
@@ -5,3 +5,7 @@ def init_app(app):
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route("/pancs", methods=["GET", "POST"])
+    def pancs():
+        return render_template("pancs.html")
