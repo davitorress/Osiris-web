4650ff97488661fed9056409b73678cba5921fc9
davitorress <dev.davitorres@gmail.com>
2023-05-23 20:57:20 -0300

    feat: route and style for panc page

--- controllers/routes.py
+++ controllers/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 
 
 def init_app(app):
@@ -9,3 +9,12 @@ def init_app(app):
     @app.route("/pancs", methods=["GET", "POST"])
     def pancs():
         return render_template("pancs.html")
+
+    @app.route("/pancs/")
+    @app.route("/pancs/<string:id>")
+    def panc(id=None):
+        if id is None or len(id) == 0:
+            return redirect(url_for("pancs"))
+        # if request.args.get("fav"):
+        # futuramente vai definir o valor de fav
+        return render_template("panc.html")
