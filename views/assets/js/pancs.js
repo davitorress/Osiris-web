9a54af84bb47ecf871b7300fdfc982e207dd4c58
davitorress <dev.davitorres@gmail.com>
2023-05-24 18:10:15 -0300

    refactor: remove console.log

--- views/assets/js/pancs.js
+++ views/assets/js/pancs.js
@@ -1,7 +1,6 @@
 const pancsItems = [...document.querySelectorAll(".panc-item")];
-console.log(pancsItems);
 
-for (pancItem of pancsItems) {
+for (const pancItem of pancsItems) {
 	pancItem.addEventListener("click", (ev) => {
 		const origin = window.location.origin;
 		window.location.href = origin + "/pancs/" + ev.currentTarget.id;
