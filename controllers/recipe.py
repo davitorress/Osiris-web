f34d92ddc494d5c54b9c746db5ae7d059d584891
davitorress <dev.davitorres@gmail.com>
2023-05-29 21:40:27 -0300

    feat: add like function and refactoring

--- controllers/recipe.py
+++ controllers/recipe.py
@@ -5,29 +5,42 @@ from bson import ObjectId
 
 class Recipe:
     def get_more_liked(db):
-        collection = db["receita"]
-        recipes = collection.find().sort("likes", -1).limit(6)
+        recipes = db["receita"].find().sort("likes", -1).limit(6)
         return recipes
 
-    def get_all(db):
-        collection = db["receita"]
-        recipes = collection.find().sort("likes", -1)
+    def get_all(db, userId):
+        recipes = list(db["receita"].find().sort("likes", -1))
+        if userId is not None:
+            favorites = db["usuario"].find_one({"_id": ObjectId(userId)}).get("salvos")
+            for item in recipes:
+                item["is_favorite"] = str(item["_id"]) in favorites
         return recipes
 
-    def get_search(db, search):
-        collection = db["receita"]
-        recipes = collection.find(
-            {
-                "$or": [
-                    {"nome": {"$regex": search, "$options": "i"}},
-                    {"pancs": {"$elemMatch": {"$regex": search, "$options": "i"}}},
-                ]
-            }
-        ).sort("likes", -1)
+    def get_search(db, userId, search):
+        recipes = list(
+            db["receita"]
+            .find(
+                {
+                    "$or": [
+                        {"nome": {"$regex": search, "$options": "i"}},
+                        {"pancs": {"$elemMatch": {"$regex": search, "$options": "i"}}},
+                    ]
+                }
+            )
+            .sort("likes", -1)
+        )
+        if userId is not None:
+            favorites = db["usuario"].find_one({"_id": ObjectId(userId)}).get("salvos")
+            for item in recipes:
+                item["is_favorite"] = str(item["_id"]) in favorites
         return recipes
 
     def get_recipe(db, id, userId):
         favorites = db["usuario"].find_one({"_id": ObjectId(userId)}).get("salvos")
         recipe = db["receita"].find_one({"_id": ObjectId(id)})
-        recipe["is_favorite"] = recipe["_id"] in favorites
+        recipe["is_favorite"] = str(recipe["_id"]) in favorites
         return recipe
+
+    def add_like(db, id):
+        likes = db["receita"].find_one({"_id": ObjectId(id)}).get("likes")
+        db["receita"].update_one({"_id": ObjectId(id)}, {"$set": {"likes": likes + 1}})
