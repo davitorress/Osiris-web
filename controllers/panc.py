ce2a3048edb12d7fbab1cfa45d04936eeed6bade
davitorress <dev.davitorres@gmail.com>
2023-05-29 21:39:36 -0300

    feat: get all names function and refactoring

--- controllers/panc.py
+++ controllers/panc.py
@@ -3,18 +3,27 @@ from bson import ObjectId
 
 class Panc:
     def get_highlight(db):
-        collection = db["panc"]
-        pancs = collection.find().limit(6)
+        pancs = db["panc"].find().limit(6)
         return pancs
 
-    def get_all(db):
-        collection = db["panc"]
-        pancs = collection.find()
+    def get_all(db, userId):
+        pancs = list(db["panc"].find())
+        if userId is not None:
+            favorites = db["usuario"].find_one({"_id": ObjectId(userId)}).get("favoritos")
+            for item in pancs:
+                item["is_favorite"] = str(item["_id"]) in favorites
         return pancs
 
-    def get_search(db, search):
-        collection = db["panc"]
-        pancs = collection.find({"nome": {"$regex": search, "$options": "i"}})
+    def get_all_names(db):
+        pancs = [doc.get("nome") for doc in db["panc"].find().sort("nome", 1)]
+        return pancs
+
+    def get_search(db, userId, search):
+        pancs = list(db["panc"].find({"nome": {"$regex": search, "$options": "i"}}))
+        if userId is not None:
+            favorites = db["usuario"].find_one({"_id": ObjectId(userId)}).get("favoritos")
+            for item in pancs:
+                item["is_favorite"] = str(item["_id"]) in favorites
         return pancs
 
     def get_panc(db, id, userId):
